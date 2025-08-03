db14aa8c0c11d8baccac22fe6f2a831b99b8dc99
Hector Encarnacion <hectorencarnacion@Hectors-MacBook-Pro.local>
2025-08-03 18:18:20 -0400

    Primer Commit

---
+++ src/componentes/Testimonio.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+function Testimonio() {
+
+  return (
+    <div className="testimonio-container">
+      <img
+        className="testimonio-imagen"
+        src={require ("../imagenes/Emma.webp")}
+        alt="Foto de Emma"
+      />
+
+
+    </div>
+
+
+
+  );
+
+}
